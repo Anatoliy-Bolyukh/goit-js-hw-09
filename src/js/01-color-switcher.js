1502d62e372288332d8a3d83a1e09ca89f2bdea0
Anatoliy-Bolyukh <tosik.bolyukh@gmail.com>
2022-08-16 22:34:43 +0300

    add fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
 
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+let timerId = "";
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+btnStartEl.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStartEl.disabled = true
+});
+
+btnStopEl.addEventListener("click", () => {
+    clearInterval(timerId);
+    btnStartEl.disabled = false
+})
